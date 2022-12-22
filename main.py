9ae57b3ec4efeca881b5885ffd77b4adf13f5627
lil-dev16 <olusegunkd2017@gmail.com>
2022-12-22 19:08:33 +0100

    rock, paper, scissors done!

--- main.py
+++ main.py
@@ -1,10 +1,32 @@
+import random
 def get_choices():
     player_choice = input("select a choice either rock, paper or scissors")
-    computer_choice = "paper"
+    options = ["rock", "paper", "scissors"]
+    computer_choice = random.choice(options)
     choices = {"player": player_choice, "computer": computer_choice}
 
     return choices
 
+def check_win(player, computer):
+    print(f"you chose {player}, computer chose {computer}")
+    if player == computer:
+        return "tie!!"
+    elif player == "rock":
+        if computer == "paper":
+            return "You lose, paper covers rock!"
+        else:
+            return "You win, rock smashes scissors!"
+    elif player == "scissors":
+        if computer == "paper":
+            return "you win, scissors cuts paper"
+        else:
+            return "you lose, rock crushes scissors"
+    elif player == "paper":
+        if computer == "rock":
+            return "you win, paper covers rock"
+        else:
+            return "you lose, scissors cuts paper"
 
-choices = get_choices()
-print(choices)
+choicxs = get_choices();
+
+print(check_win(choicxs["player"], choicxs["computer"]));
