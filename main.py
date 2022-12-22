1ae77cf44d3acbcf7223c0a1209aa596045f9d9c
lil-dev16 <olusegunkd2017@gmail.com>
2022-12-22 04:44:15 +0100

    inputs

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 def get_choices():
-    player_choice = "rock"
+    player_choice = input("select a choice either rock, paper or scissors")
     computer_choice = "paper"
+    choices = {"player": player_choice, "computer": computer_choice}
 
-    return computer_choice
+    return choices
+
 
 choices = get_choices()
 print(choices)
